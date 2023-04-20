8d3c2ea209f499606bf62040b1c68d5787e18b5d
aarongerbrandt <aarond.gerbrandt@gmail.com>
2023-04-20 17:49:39 -0500

    Added timer to color changing effect

--- source/ScreensaverHero.hx
+++ source/ScreensaverHero.hx
@@ -16,8 +16,12 @@ class ScreensaverHero extends FlxSprite {
     private static var MIN_HEX_COLOR(default, null) = 0;
     private static var MAX_HEX_COLOR(default, null) = 16777216;
 
+    private static var COLOR_CHANGE_TIME_SECONDS(default, null) = 2.5;
+
     var velY = FlxG.random.int(15,25) * FlxG.random.sign();
     var velX = FlxG.random.int(15,25) * FlxG.random.sign();
+
+    var colorChangeTimer:Float;
 
     public function new(x=0,y=0)
     {
@@ -26,10 +30,12 @@ class ScreensaverHero extends FlxSprite {
         this.x = x;
         this.y = y;
 
-        this.velocity.x = velY;
-        this.velocity.y = velX;
+        colorChangeTimer = COLOR_CHANGE_TIME_SECONDS;
 
-        this.color = FlxG.random.int(MIN_HEX_COLOR, MAX_HEX_COLOR);
+        velocity.x = velY;
+        velocity.y = velX;
+
+        color = FlxG.random.int(MIN_HEX_COLOR, MAX_HEX_COLOR);
     }
 
     override public function update(elapsed:Float)
@@ -38,12 +44,19 @@ class ScreensaverHero extends FlxSprite {
         FlxSpriteUtil.screenWrap(this);
 
         // Sign will make it randomly increase OR decrease by that amount
-        var newColor = (this.color + Std.int(4*(this.x + this.y))) * FlxG.random.sign();    
         // Should prevent from reaching a crazy number I think?
-        this.color = newColor % MAX_HEX_COLOR;          
+        
 
         if (getDistanceFromMouse(FlxG.mouse) < MOUSE_FLEE_DISTANCE) {
             FlxVelocity.moveTowardsMouse(this, MOUSE_FLEE_SPEED, MOUSE_FLEE_TIMEOUT_MS);
+        }
+
+        colorChangeTimer -= elapsed;
+        if(colorChangeTimer <= 0) {
+            var newColor = (this.color + Std.int(4*(this.x + this.y))) * FlxG.random.sign();    
+            color = newColor % MAX_HEX_COLOR;
+
+            colorChangeTimer = COLOR_CHANGE_TIME_SECONDS;
         }
     }
 
